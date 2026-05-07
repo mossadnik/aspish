c6dd0da99b3df943c48e87821ba3744f0f0db842
Matthias Ossadnik <ossadnik.matthias@gmail.com>
2026-05-07 14:29:41 +0200

    add __version__

--- src/aspish/__init__.py
+++ src/aspish/__init__.py
@@ -1,8 +1,19 @@
 from .solver import Solver
-from .language import var, not_
+from .language import (
+    var,
+    predicate,
+    not_,
+    BLANK,
+)
+
+
+__version__ = '0.1.0'
+
 
 __all__ = [
     'Solver',
     'var',
+    'predicate',
     'not_',
+    'BLANK',
 ]
